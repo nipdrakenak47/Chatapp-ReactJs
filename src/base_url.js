2733816149f6a8534541e114ba38c665d889f09b
kaushal Kalal <nipdrakenak47@gmail.com>
2024-04-13 20:59:15 +0530

    final url set

--- src/base_url.js
+++ src/base_url.js
@@ -1,5 +1,5 @@
 
-// export const BASE_URL = 'https://chatapp-spring-boot.onrender.com';
+export const BASE_URL = 'https://chatapp-spring-boot.onrender.com';
 
-export const BASE_URL = 'http://chat-app-spring-boot-env.eba-ryrcvndz.ap-southeast-2.elasticbeanstalk.com';
+
 
